4c53f38fd7296f43818d8c876a3fad55d0c08ecb
JoseVara <75636446+JoseVaraQuispe@users.noreply.github.com>
2023-09-27 13:46:15 -0500

    registerlogin

--- app/src/main/java/com/example/reciperealm/MainActivity.kt
+++ app/src/main/java/com/example/reciperealm/MainActivity.kt
@@ -1,11 +1,72 @@
 package com.example.reciperealm
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val txtCorreo: EditText =findViewById(R.id.txtCorreo)
+        val txtPass: EditText =findViewById(R.id.txtPass)
+        val btnIniciar: Button =findViewById(R.id.btnIniciar)
+        val btnRegistrar: Button =findViewById(R.id.btnRegistrar)
+        val chkRecordar: CheckBox=findViewById(R.id.chkRecordar)
+        val txtMsg: TextView=findViewById(R.id.txtMsg)
+
+        val pref: SharedPreferences =this.getSharedPreferences("pref1", Context.MODE_PRIVATE)
+        val check:Boolean=pref.getBoolean("chk",false)
+        val usu:String=pref.getString("usu","")!!
+        val pass:String=pref.getString("pass","")!!
+
+        if(check){
+            txtCorreo.setText(usu)
+            txtPass.setText(pass)
+            chkRecordar.isChecked=true
+        }else{
+            txtCorreo.setText(usu)
+            txtPass.setText(pass)
+            chkRecordar.isChecked=false
+        }
+
+        btnIniciar.setOnClickListener(){
+            val usu:String=txtCorreo.text.toString()
+            val pass:String=txtPass.text.toString()
+
+            val editor:SharedPreferences.Editor=pref.edit()
+            if(usu == "upc@upc.pe" && pass== "upc"){
+                if(chkRecordar.isChecked){
+                    editor.putString("usu",usu)
+                    editor.putString("pass",pass)
+                    editor.putBoolean("chk",true)
+                }else{
+                    editor.putString("usu","")
+                    editor.putString("pass","")
+                    editor.putBoolean("chk",false)
+                }
+                editor.apply()
+                editor.commit()
+
+                //cambiar a la pagina principal
+                //val intent= Intent(this, MainActivity2::class.java)
+                //startActivity(intent)
+            }else{
+                txtCorreo.text.clear()
+                txtPass.text.clear()
+                txtMsg.text="Usuario o Pass incorrectos"
+            }
+        }
+
+        btnRegistrar.setOnClickListener(){
+            val intent= Intent(this, RegisterView::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/reciperealm/RegisterView.kt
+++ app/src/main/java/com/example/reciperealm/RegisterView.kt
@@ -1,11 +1,46 @@
 package com.example.reciperealm
 
+import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.TextView
 
 class RegisterView : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register_view)
+
+        val txtCorreo: EditText =findViewById(R.id.txtRegCorreo)
+        val txtPass: EditText =findViewById(R.id.txtRegPass)
+        val txtNom: EditText =findViewById(R.id.txtRegNom)
+        val txtApe: EditText =findViewById(R.id.txtRegApe)
+        val txtMsg: TextView=findViewById(R.id.txtRegMsg)
+
+        val btnRegistrar: Button =findViewById(R.id.btnRegRegistrar)
+        val btnRegresar: Button =findViewById(R.id.btnRegCancelar)
+
+
+        btnRegistrar.setOnClickListener(){
+            val nom:String=txtNom.text.toString()
+            val ape:String=txtApe.text.toString()
+            val usu:String=txtCorreo.text.toString()
+            val pass:String=txtPass.text.toString()
+            if (nom === "" || ape === "" || usu === "" || pass === ""){
+                txtMsg.text="Todos los campos son obligatorios"
+            }else{
+                // funcion para enviar el registro
+                val intent= Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }
+        }
+
+        btnRegresar.setOnClickListener(){
+            val intent= Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
