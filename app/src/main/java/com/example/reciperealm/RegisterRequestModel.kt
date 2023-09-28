b4b0371f44af7c9d265009fa155ed469383f6745
JoseVara <75636446+JoseVaraQuispe@users.noreply.github.com>
2023-09-28 15:18:25 -0500

    reuest de registrar y login

---
+++ app/src/main/java/com/example/reciperealm/RegisterRequestModel.kt
@@ -0,0 +1,3 @@
+package com.example.reciperealm
+
+data class RegisterRequestModel()
---
+++ app/src/main/java/com/example/reciperealm/ResponseLoginClass.kt
@@ -0,0 +1,6 @@
+package com.example.reciperealm
+
+data class ResponseSuccessLoginClass(
+    val access_token:String,
+    val refresh_token:String
+)
